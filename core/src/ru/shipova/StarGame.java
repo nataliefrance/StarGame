110e3b5ff784ce02315e160535599a9a83db33d3
Natalia Shipova <nataliefrance@mail.ru>
2019-05-20 15:19:15 +0300

    Урок 2: простейшие операции с векторами

--- core/src/ru/shipova/StarGame.java
+++ core/src/ru/shipova/StarGame.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Vector;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -14,6 +17,25 @@ public class StarGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("background.jpg");
+		Vector2 v1 = new Vector2(2,3);
+		Vector2 v2 = new Vector2(0,-1);
+		v1.add(v2);
+		System.out.println("add: " + v1.x + " " + v1.y);
+
+		v1.set(7, 10);
+		v2.set(3, 3);
+		v1.sub(v2);
+		System.out.println("sub: " + v1.x + " " + v1.y);
+
+		Vector2 v3 = new Vector2(4, 6);
+		v3.scl(0.9f);
+		System.out.println("v3: " + v3.x + " " + v3.y);
+
+		//вычисляем угол между векторами в радианах
+		v1.nor();
+		v2.nor();
+        System.out.println(Math.acos(v1.dot(v2)));
+
 	}
 
 	@Override
