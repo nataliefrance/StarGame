b91529422039ccc38b83394fe192d3e366b8c4f9
Natalia Shipova <nataliefrance@mail.ru>
2019-05-22 19:42:33 +0300

    Урок 3

--- core/src/ru/shipova/base/BaseScreen.java
+++ core/src/ru/shipova/base/BaseScreen.java
@@ -3,11 +3,37 @@ package ru.shipova.base;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.shipova.math.MatrixUtils;
+import ru.shipova.math.Rect;
+
 
 public abstract class BaseScreen implements Screen, InputProcessor {
+
+    protected SpriteBatch batch;
+    private Vector2 touch;
+    private Rect screenBounds;
+    private Rect worldBounds;
+    private Rect glBounds;
+
+    private Matrix4 worldToGl;
+    private Matrix3 screenToWorld;
+
     @Override
     public void show() {
         Gdx.input.setInputProcessor(this);
+        this.batch = new SpriteBatch();
+        this.touch = new Vector2();
+        this.screenBounds = new Rect();
+        this.worldBounds = new Rect();
+        this.glBounds = new Rect(0, 0, 1f, 1f);
+
+        this.worldToGl = new Matrix4();
+        this.screenToWorld = new Matrix3();
     }
 
     //работает 60 раз в секунду
@@ -19,6 +45,20 @@ public abstract class BaseScreen implements Screen, InputProcessor {
     //изменили размер экрана
     @Override
     public void resize(int width, int height) {
+        screenBounds.setSize(width, height);
+        screenBounds.setLeft(0);
+        screenBounds.setBottom(0);
+
+        float aspect = width / (float) height;
+        worldBounds.setHeight(1f);
+        worldBounds.setWidth(1f * aspect);
+        MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
+        batch.setProjectionMatrix(worldToGl);
+        MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
+        resize(worldBounds);
+    }
+
+    public void resize(Rect worldBounds) {
 
     }
 
@@ -42,7 +82,7 @@ public abstract class BaseScreen implements Screen, InputProcessor {
 
     @Override
     public void dispose() {
-
+        batch.dispose();
     }
 
     //нажали кнопку
@@ -67,20 +107,37 @@ public abstract class BaseScreen implements Screen, InputProcessor {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDown(touch, pointer);
+        System.out.println(touch.x + " " + touch.y);
+        return false;
+    }
+
+    public boolean touchDown(Vector2 touch, int pointer) {
         return false;
     }
 
     //отпустили тач или клик мыши, координаты мыши или пальца
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchUp(touch, pointer);
+        return false;
+    }
 
+    public boolean touchUp(Vector2 touch, int pointer) {
         return false;
     }
 
     //тапнули в одном месте, протащили по экрану, отпустили в другом месте
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
+        touch.set(screenX, screenBounds.getHeight() - screenY).mul(screenToWorld);
+        touchDragged(touch, pointer);
+        return false;
+    }
 
+    public boolean touchDragged(Vector2 touch, int pointer) {
         return false;
     }
 
--- core/src/ru/shipova/screen/MenuScreen.java
+++ core/src/ru/shipova/screen/MenuScreen.java
@@ -3,79 +3,60 @@ package ru.shipova.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.shipova.base.BaseScreen;
+import ru.shipova.math.Rect;
+import ru.shipova.sprite.Background;
+import ru.shipova.sprite.SpaceCat;
 
 public class MenuScreen extends BaseScreen {
 
-    private SpriteBatch batch;
-    private Texture img;
-    private Vector2 touch;
-    private Vector2 speed;
-    private Vector2 position;
-    private float rightBorder;
-    private float topBorder;
-    private float leftBorder;
-    private float bottomBorder;
-    private float speedX;
-    private float speedY;
-    private Vector2 direction;
-    private boolean isTouched;
+    private Texture bg;
+    private Background background;
+    private SpaceCat spaceCat;
+    private Texture sc_texture;
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
-        img = new Texture("cat.jpg");
-        touch = new Vector2();
-        speedX = 5f;
-        speedY = 5f;
-        speed = new Vector2(speedX, speedY);
-        position = new Vector2();
-        direction = new Vector2();
+        bg = new Texture("background.png");
+        background = new Background(new TextureRegion(bg));
+        sc_texture = new Texture("cat.png");
+        spaceCat = new SpaceCat(new TextureRegion(sc_texture));
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        Gdx.gl.glClearColor(0, 1, 1, 1);//цвет фона
+        super.render(delta);
+        Gdx.gl.glClearColor(0.4f, 0.3f, 0.9f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, position.x, position.y);
+        background.draw(batch);
+        spaceCat.draw(batch);
         batch.end();
-        rightBorder = position.x + img.getWidth();
-        topBorder = position.y + img.getHeight();
-        leftBorder = position.x;
-        bottomBorder = position.y;
-
-        if (isTouched){
-            direction.x = touch.x - position.x;
-            direction.y = touch.y - position.y;
-            direction.nor();
-            speed.scl(direction);
-            position.add(speed);
-            speed.set(speedX, speedY);
-                if (Math.abs(touch.x - position.x) < 1 || Math.abs(touch.y - position.y) < 1){
-                    isTouched = false;
-                }
-        }
-
-        //картинка катается туда-сюда
-//        if (leftBorder >= 0 && bottomBorder >= 0) {
-//            if (rightBorder < Gdx.graphics.getWidth() && topBorder < Gdx.graphics.getHeight()) {
-//                position.add(speed);
-//            } else speed.set(-speedX, -speedY);
-//            position.add(speed);
-//        } else speed.set(speedX, speedY);
-//        position.add(speed);
 
     }
 
     @Override
-    public void resize(int width, int height) {
-        super.resize(width, height);
+    public void dispose() {
+        bg.dispose();
+        sc_texture.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        spaceCat.resize(worldBounds);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        return super.touchDown(touch, pointer);
     }
 
     @Override
@@ -93,19 +74,5 @@ public class MenuScreen extends BaseScreen {
         super.hide();
     }
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
-        super.dispose();
-    }
 
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        super.touchDown(screenX, screenY, pointer, button);
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        System.out.println("touch.x = " + touch.x + " touch.y = " + touch.y);
-        isTouched = true;
-        return false;
-    }
 }
